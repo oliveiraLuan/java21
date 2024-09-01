226f2490394da2659b391d0076afabb217d11f87
Luan de Oliveira <luan012012@hotmail.com>
2024-08-31 23:27:44 -0300

    feat: criado classe demo com isBlank, repeat e indent

---
+++ NewStringMethods/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Demo {
+    private static final String text = "Debra Morgan";
+
+    public static void main(String[] args) {
+        //Verifica se o texto está em branco ou nulo
+        System.out.println(STR."IsBlank = \{text.isBlank()}");
+        //Repete a string de acordo com parâmetro count
+        System.out.println("So".repeat(2));
+        //Faz a identação com parâmetro n
+        System.out.println("Format line".indent(4));
+    }
+}
